c75bd5ebfeb649c6deb019462652899e576dbb9c
Hayaki Saito <user@zuse.jp>
2014-01-26 04:00:21 +0900

    Update setup script for building c module

--- setup.py
+++ setup.py
@@ -11,9 +11,11 @@ long_description = open(os.path.join(dirpath, "README.rst")).read()
 
 setup(name                  = 'PySixel',
       version               = __version__,
-      description           = 'Make SIXEL color graphics supported by some terminal emulators(DECTerm/RLogin/mlterm/tanasinn/xterm)',
+      description           = ('Make SIXEL color graphics supported by some '
+                               'terminal emulators(DECTerm/RLogin/mlterm/tanasinn/xterm)'),
       long_description      = long_description,
       py_modules            = ['sixel'],
+      ext_modules           = [Extension('sixel_cimpl', sources = ['sixel/sixel_cimpl.c'])],
       eager_resources       = [],
       classifiers           = ['Development Status :: 4 - Beta',
                                'Topic :: Terminals',
@@ -22,13 +24,13 @@ setup(name                  = 'PySixel',
                                'License :: OSI Approved :: GNU General Public License (GPL)',
                                'Programming Language :: Python'
                                ],
-      keywords              = 'sixel terminal',
+      keywords              = 'sixel terminal image',
       author                = __author__,
       author_email          = 'user@zuse.jp',
       url                   = 'https://github.com/saitoha/PySixel',
       license               = __license__,
       packages              = find_packages(exclude=[]),
-      zip_safe              = True,
+      zip_safe              = False,
       include_package_data  = False,
       install_requires      = ['imageloader', 'PIL'],
       entry_points          = """
